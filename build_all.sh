245113eeddcd5e506a85c9904db486533282410d
Chris Webster <cjw@ucar.edu>
2018-10-31 08:30:28 -0600

    Add build_all script and submodules
    Add each project we want built for the nightly build.
    Add a build_all script.

---
+++ build_all.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+if [ -z $JLOCAL ]; then
+	echo "Environment variable JLOCAL not set, setting to $JLOCAL."
+	export JLOCAL=/opt/local
+fi
+
+#
+# Make sure libraf and vardb are done early
+#
+for repo in libraf ncplot
+do
+	cd $repo
+	make install
+	cd ..
+done
+
+#
+# scons targets with the --prefix option
+#
+for repo in vardb ncpp oap nc_utils
+do
+	cd $repo
+	scons --prefix=$JLOCAL
+	scons --prefix=$JLOCAL install
+	cd ..
+done
+
+#
+# scons targets with the --prefix option
+#
+for repo in caledit
+do
+	cd $repo
+	scons
+	scons install
+	cd ..
+done
